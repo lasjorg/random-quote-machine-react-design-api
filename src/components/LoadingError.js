326ee42996f66a68cf49fa1ddfab8a0363eb4a35
lasjorg <28780271+lasjorg@users.noreply.github.com>
2019-07-13 19:18:08 +0200

    update error message

--- src/components/LoadingError.js
+++ src/components/LoadingError.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const LoadingError = () => {
-  return <div className="error">Something went wrong</div>;
+  return <div className="error">Something went wrong, please try again</div>;
 };
 
 export default LoadingError;
